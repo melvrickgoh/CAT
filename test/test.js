7607092b693795a6ebc7e723560c4e86df9ae2a9
Melvrick <melvrickgoh@hotmail.com>
2014-06-23 18:33:55 +0800

    done setting up mocha

---
+++ test/test.js
@@ -0,0 +1,26 @@
+
+var after = require('after');
+var express = require('express'),
+  User = require('../server/entity/user'),
+  methods = require('methods'),
+  assert = require('assert');
+
+describe('User', function(){
+  var mel = new User({
+    username:"Mel",
+    email:"melvrickgoh@hotmail.com",
+    lastAttemptedLesson:"hello"
+  });
+
+  console.log(mel);
+
+  it('should be able to call username',function(){
+    assert.equal('Mel',mel.getUsername());
+    //done();
+  });
+
+  it('should be able to call email',function(){
+    assert.equal('melvrickgoh@hotmail.com',mel.getEmail());
+    //done();
+  });
+});
