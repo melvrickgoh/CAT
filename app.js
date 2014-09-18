4b127dddde57cd12d0a97ac335201f43e7b5433c
Melvrick <melvrickgoh@hotmail.com>
2014-09-18 14:06:10 +0800

    changed back to app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ var router = require('./server');
 var app = express();
 
 // all environments
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080, "127.0.0.1");
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ipaddress', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 // config
 app.set('views', __dirname + '/client/view');
